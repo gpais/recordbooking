c0c290d25bf4d7e68a42127ff236b89f9740ae99
moreira_pais@hotmail.com <moreira_pais@hotmail.com>
2016-09-25 19:19:29 +0100

    <commit message>

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,5 +1,32 @@
 (function(angular) {
-  angular.module("myApp.controllers", []);
-  angular.module("myApp.services", []);
-  angular.module("myApp", ["ngResource", "myApp.controllers", "myApp.services"]);
+  angular.module("myApp.controllers.reservations", []);
+  angular.module("myApp.services.reservations", []);
+  angular.module("myApp.services.payments", []);
+  angular.module("myApp.controllers.payments", []);
+ 
+  var routerApp= angular.module("myApp", ["ngResource","ui.router", "myApp.controllers.reservations", "myApp.services.reservations","myApp.controllers.payments", "myApp.services.payments"]);
+  
+  routerApp.config(function($stateProvider, $urlRouterProvider) {
+	    
+	    $urlRouterProvider.otherwise('/reserve');
+	    
+	    $stateProvider
+	        
+	        // HOME STATES AND NESTED VIEWS ========================================
+	        .state('reserve', {
+	            url: '/reserve',
+	            templateUrl: 'Reservation.html'
+	        })
+	        
+	         .state('payment', {
+	            url: '/payment',
+	            templateUrl: 'Payment.html'
+	        })
+	        
+	        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+	        .state('about', {
+	            // we'll get to this in a bit       
+	        });
+	        
+	});
 }(angular));
